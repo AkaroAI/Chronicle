ce3dd8cd2362e980a5668c778bc8d6a461374a5c
AkaroAI <patrickpatpaz@gmail.com>
2026-08-17 23:11:56 -0700

    Update ProviderSettingsDialog.kt

--- app/src/main/java/com/akaroai/chronicle/ui/ProviderSettingsDialog.kt
+++ app/src/main/java/com/akaroai/chronicle/ui/ProviderSettingsDialog.kt
@@ -4,6 +4,7 @@ import androidx.compose.foundation.layout.*
 import androidx.compose.material3.*
 import androidx.compose.runtime.*
 import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
 import androidx.compose.ui.text.input.PasswordVisualTransformation
 import androidx.compose.ui.unit.dp
 import com.akaroai.chronicle.provider.ProviderSettings
