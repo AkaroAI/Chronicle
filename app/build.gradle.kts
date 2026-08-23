a6fe1660794e1870dfc67b4a03d22b690cc71b8e
AkaroAI <patrickpatpaz@gmail.com>
2026-08-23 07:19:03 -0700

    Run proposal sanitizer tests with JSON runtime

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -70,4 +70,5 @@ dependencies {
     implementation("org.jetbrains.kotlinx:kotlinx-coroutines-android:1.9.0")
     implementation("com.squareup.okhttp3:okhttp:4.12.0")
     testImplementation("junit:junit:4.13.2")
+    testImplementation("org.json:json:20240303")
 }
